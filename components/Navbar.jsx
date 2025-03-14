c47a0913f76804b3c6c8dd7d07c4af7f47040b23
humayun167 <humayunrashidkhan96@gmail.com>
2025-03-15 03:20:54 +0600

    authentication done

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,8 +4,11 @@ import { assets} from "@/assets/assets";
 import Link from "next/link"
 import { useAppContext } from "@/context/AppContext";
 import Image from "next/image";
+import { useClerk } from "@clerk/nextjs";
 
 const Navbar = () => {
+
+  const{openSignIn } = useClerk()
 
   const { isSeller, router } = useAppContext();
 
@@ -37,7 +40,7 @@ const Navbar = () => {
 
       <ul className="hidden md:flex items-center gap-4 ">
         <Image className="w-4 h-4" src={assets.search_icon} alt="search icon" />
-        <button className="flex items-center gap-2 hover:text-gray-900 transition">
+        <button onClick={openSignIn}  className="flex items-center gap-2 hover:text-gray-900 transition">
           <Image src={assets.user_icon} alt="user icon" />
           Account
         </button>
@@ -45,7 +48,7 @@ const Navbar = () => {
 
       <div className="flex items-center md:hidden gap-3">
         {isSeller && <button onClick={() => router.push('/seller')} className="text-xs border px-4 py-1.5 rounded-full">Seller Dashboard</button>}
-        <button className="flex items-center gap-2 hover:text-gray-900 transition">
+        <button onClick={openSignIn} className="flex items-center gap-2 hover:text-gray-900 transition">
           <Image src={assets.user_icon} alt="user icon" />
           Account
         </button>
