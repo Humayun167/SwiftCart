c655880d66671a7580e5dd162aac383ceaa2d9ab
humayun167 <humayunrashidkhan96@gmail.com>
2025-03-16 01:40:25 +0600

    frontend done

---
+++ models/User.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+
+    _id: {type: String, required: true},
+    name: {type: String, required: true},
+    email: {type: String, required: true},
+    imgUrl: {type: String, required: true},
+    cartItems: {type: Object, required: true},
+
+}, {minimize: false});
+
+
+const user = mongoose.models.user || mongoose.model('user', userSchema);
+
+export default User;
