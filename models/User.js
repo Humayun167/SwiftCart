4215e77edaad79de286e010befd1c61eb003183c
humayun167 <humayunrashidkhan96@gmail.com>
2025-03-17 01:55:06 +0600

    check

--- models/User.js
+++ models/User.js
@@ -11,6 +11,6 @@ const userSchema = new mongoose.Schema({
 }, {minimize: false});
 
 
-const user = mongoose.models.user || mongoose.model('user', userSchema);
+const User = mongoose.models.user || mongoose.model('user', userSchema);
 
 export default User;
